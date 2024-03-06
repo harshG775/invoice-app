3416a65c1fa40946c989e6770706bc030c8364e6
Harsh <hgaur491@gmail.com>
2024-03-06 10:41:50 +0530

    fixes

--- src/app/invoice/layout.tsx
+++ src/app/invoice/layout.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 
@@ -6,12 +6,14 @@ type SubItem = {
     id: number;
     name: string;
     path: string;
+    iconName: string;
 };
 type Item = {
     id: number;
     name: string;
     path?: string;
-	subItem?: SubItem[]
+    iconName: string;
+    subItem: SubItem[];
 };
 
 const sidebarData = [
@@ -19,66 +21,75 @@ const sidebarData = [
         id: 1,
         name: "Invoice",
         path: "/",
+        iconName: "",
+        subItem: [],
     },
     {
-        id: 1,
+        id: 2,
         name: "Sales",
         path: "/sales",
+        iconName: "",
+        subItem: [],
     },
     {
-        id: 1,
+        id: 3,
         name: "Inventory",
-		subItem: [
-			{
-				id: 1,
-				name: "Items",
-				path: "/items",
-			},
-			{
-				id: 1,
-				name: "Services",
-				path: "/services",
-			},
-		],
+        iconName: "",
+        subItem: [
+            {
+                id: 1,
+                name: "Items",
+                path: "/items",
+                iconName: "",
+            },
+            {
+                id: 2,
+                name: "Services",
+                path: "/services",
+                iconName: "",
+            },
+        ],
     },
-]
-function SubItem({name, path}: SubItem) {
-	return (
-		<li>
-			<Link href={path} className="w-full inline-block">
-				{name}
-			</Link>
-		</li>
-	);
+];
+function SubItem({ name, path }: SubItem) {
+    return (
+        <li>
+            <Link href={path} className="w-full inline-block">
+                {name}
+            </Link>
+        </li>
+    );
 }
-function SidebarItem({name, path, subItem}: Item) {
-	if (path && !subItem) {
-		return (
-			<li>
-				<Link href={path} className="w-full inline-block">
-					{name}
-				</Link>
-			</li>
-		);
-	}
-	if (subItem && subItem.length > 0) {
-		return (
-			<li>
-				<button className="w-full inline-block text-left">
-					{name}
-				</button>
-				<ul>
-					<SubItem {...subItem[0]}/>
-				</ul>
-			</li>
-		);
-	}
+function SidebarItem({ name, path, subItem }: Item) {
+    if (path) {
+        return (
+            <li>
+                <Link href={path} className="w-full inline-block">
+                    {name}
+                </Link>
+            </li>
+        );
+    }
+    if (subItem.length > 0) {
+        return (
+            <li>
+                <button className="w-full inline-block text-left">
+                    {name}
+                </button>
+                <ul>
+					{subItem.map((subItem: SubItem) => (
+						<SubItem key={subItem.id} {...subItem} />
+					))}
+                </ul>
+            </li>
+        );
+    }
 }
 function InvoiceSidebar() {
-	return (
+    return (
         <div className="bg-neutral-800 text-neutral-50 fixed top-0 left-0 bottom-0 w-64">
             <ul className="h-full overflow-y-scroll scrollbar-thin">
-                {sidebarData.map((item:Item) => (
+                {sidebarData.map((item: Item) => (
                     <SidebarItem key={item.id} {...item} />
                 ))}
             </ul>
@@ -87,9 +98,10 @@ function InvoiceSidebar() {
     );
 }
 
-
-export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
-	return (
+export default function RootLayout({
+    children,
+}: Readonly<{ children: React.ReactNode }>) {
+    return (
         <>
             <InvoiceSidebar />
             <div className="pl-64">{children}</div>
